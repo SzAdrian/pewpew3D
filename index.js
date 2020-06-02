be47b81f43e0f270677f3b426ba9f3f2c76594bb
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-06-02 14:09:21 +0200

    :cry:

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Socketio = require("socket.io")(Http, {
   handlePreflightRequest: (req, res) => {
     const headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+      "Access-Control-Allow-Origin": "*", //or the specific origin you want to give access to,
       "Access-Control-Allow-Credentials": true,
     };
     res.writeHead(200, headers);
