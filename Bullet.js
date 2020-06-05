6f5cbd7486c4322334e163f382d6dd4d53868260
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-06-05 21:51:12 +0200

    Bullet and Player class extracted into files

---
+++ Bullet.js
@@ -0,0 +1,21 @@
+class Bullet {
+  constructor(playerX, playerY, angle, socket) {
+    this.x = playerX;
+    this.y = playerY;
+    this.angle = angle;
+    this.socket = socket;
+    this.speed = 3;
+    this.damage = 3;
+  }
+
+  move() {
+    this.x = this.x + this.speed * Math.cos((Math.PI * this.angle) / 180);
+    this.y = this.y + this.speed * Math.sin((Math.PI * this.angle) / 180);
+  }
+
+  wallCollision() {
+    return this.x < 0 || this.x > 640 || this.y < 0 || this.y > 480;
+  }
+}
+
+module.exports = Bullet;
---
+++ Player.js
@@ -0,0 +1,55 @@
+const { rndInt } = require("./UtilStuff");
+
+class Player {
+  constructor() {
+    this.x = rndInt(0, 640);
+    this.y = rndInt(0, 480);
+    this.name = `Player${rndInt(0, 480)}`;
+    this.health = 100;
+    this.velY = 0;
+    this.velX = 0;
+    this.speed = 2;
+    this.friction = 0.96;
+    this.angle = 0;
+    this.moves = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    };
+  }
+  move() {
+    if (this.moves["up"]) {
+      if (this.velY > -this.speed) {
+        this.velY--;
+      }
+    }
+
+    if (this.moves["down"]) {
+      if (this.velY < this.speed) {
+        this.velY++;
+      }
+    }
+    if (this.moves["right"]) {
+      if (this.velX < this.speed) {
+        this.velX++;
+      }
+    }
+    if (this.moves["left"]) {
+      if (this.velX > -this.speed) {
+        this.velX--;
+      }
+    }
+
+    this.velY *= this.friction;
+    this.y += this.velY;
+    this.velX *= this.friction;
+    this.x += this.velX;
+  }
+
+  bulletHit(bullet) {
+    this.health -= bullet.damage;
+  }
+}
+
+module.exports = Player;
